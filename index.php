e30a6cdb17ffa51719b156a9b76abbd52cc28ad8
Violentboy <tkachenkomaks1@gmail.com>
2018-12-06 11:50:13 +0200

    Переменные и области видимости

--- index.php
+++ index.php
@@ -1,12 +1,30 @@
+<!DOCTYPE html>
 <meta charset="utf-8">
 
 <?php
-//Комментарий в одну строку
-/*
-Многострочный комментарий
-*/
-$reg = 'Регистр 1';
-$reG = 'Регистр 2';
-$string = $reg. '<br>'. $reG;
-echo $string;
+error_reporting(E_ALL);
+//$x,$y Глобальный переменные, которые объявляются вне всяких функций
+$x = 10;
+$y = 15;
+function test() {
+    global $y, $x;
+    echo $y. '<br>';
+    echo $x. '<br>';
+}
+test();
 
+//Ключевое слово static. Она не переопределяется при каждом вызове функции, 
+//Она сохраняется в памяти!!
+function test1() {
+    static $z = 0;
+    $z++;
+    echo $z. '<br>';
+}
+test1();
+test1();
+test1();
+
+//Вызываем переменную $x через глобальный массив $GLOBALS
+echo $GLOBALS['x'];
+echo '<pre>';
+print_r($GLOBALS); //Распечатать массив $GLOBALS
